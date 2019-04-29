9810ca60fc1df218d9983f4ffc727196aba559d1
Shahryar Ahmed <42492081+ShahryarAhmed97@users.noreply.github.com>
2019-04-29 19:01:07 +0500

    Updates

---
+++ precache-manifest.0a278d73c831c8edf354c7776179f190.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "016d6bec5439b0a132cce26a5840f2cd",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "ed1ca484a970142bc3b4",
+    "url": "/repository-name/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "38e088a08f1b6e8db14a",
+    "url": "/repository-name/static/js/2.1146f8e0.chunk.js"
+  },
+  {
+    "revision": "ed1ca484a970142bc3b4",
+    "url": "/repository-name/static/js/main.fc62576a.chunk.js"
+  },
+  {
+    "revision": "6e2fc4b7d77a9c53df47",
+    "url": "/repository-name/static/js/runtime~main.f7634681.js"
+  }
+]);
